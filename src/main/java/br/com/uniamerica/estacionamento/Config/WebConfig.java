90b335b0cad08f9891d7ea7ef9588f5abd9f60b3
tailyne-bertoncelli <tailyne.berton@gmail.com>
2023-06-21 16:21:58 -0300

    Alterando configurações

--- src/main/java/br/com/uniamerica/estacionamento/Config/WebConfig.java
+++ src/main/java/br/com/uniamerica/estacionamento/Config/WebConfig.java
@@ -1,4 +1,5 @@
 package br.com.uniamerica.estacionamento.Config;
+import ch.qos.logback.core.model.processor.AllowAllModelFilter;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistration;
@@ -9,7 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+    @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**");
+        registry.addMapping("/***")
+                .allowedOrigins("*")
+                .allowedMethods("*");
     }
 }
